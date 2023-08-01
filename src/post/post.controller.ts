ebdcdeadc0ec701db544e854af84f9fa3b238bf4
Kaung Myat Han <arkarhtethan@gmail.com>
2023-08-01 12:01:36 +0630

    feat(): post module crud finished

---
+++ src/post/post.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
+import { PostService } from './post.service';
+
+@Controller('post')
+export class PostController {
+  constructor(private readonly postService: PostService) {}
+
+  @Post()
+  create(@Body() createPostDto: CreatePostDto) {
+    return this.postService.createPost(createPostDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.postService.getAllPosts();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.postService.getPostById(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
+    return this.postService.updatePost(+id, updatePostDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.postService.deletePost(+id);
+  }
+}
