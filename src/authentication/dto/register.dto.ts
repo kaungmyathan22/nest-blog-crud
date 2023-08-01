9d11ad15e7a52e06ede79812f7664eff6ef7aac1
Kaung Myat Han <arkarhtethan@gmail.com>
2023-08-01 14:22:53 +0630

    chore(): validation setup

--- src/authentication/dto/register.dto.ts
+++ src/authentication/dto/register.dto.ts
@@ -1,6 +1,11 @@
+import { IsEmail, IsString, IsStrongPassword } from 'class-validator';
+
 export class RegisterDto {
+  @IsEmail()
   email: string;
+  @IsString()
   name: string;
+  @IsStrongPassword()
   password: string;
 }
 
